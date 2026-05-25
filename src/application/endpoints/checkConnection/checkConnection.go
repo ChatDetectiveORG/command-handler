3a2590919ec4e9ff7c5789948266573dccba2a47
bloodofspring <t.shagin@gmail.com>
2026-05-25 18:41:38 +0300

    Feat: Refactored CheckConnection Endpoint

--- src/application/endpoints/checkConnection/checkConnection.go
+++ src/application/endpoints/checkConnection/checkConnection.go
@@ -7,10 +7,11 @@ import (
 	"github.com/ChatDetectiveORG/command-handler/src/infrastructure/postgresql"
 	e "github.com/ChatDetectiveORG/shared/errors"
 	h "github.com/ChatDetectiveORG/shared/handlers"
-	utils "github.com/ChatDetectiveORG/shared/utils"
+	"github.com/ChatDetectiveORG/shared/telegram"
 	tele "gopkg.in/telebot.v4"
 )
 
+// Send user info about bot status
 func NewCheckConnectionEndpoint() h.Endpoint {
 	ep := h.Endpoint{}
 	ep.Init(
@@ -27,40 +28,54 @@ func NewCheckConnectionEndpoint() h.Endpoint {
 	return ep
 }
 
+// Build message about bot connected
+//
+// Takes: chat ID
+//
+// Returns: message
 func buildConnectedMessage(chatID int64) *tele.Message {
-	return &tele.Message{
-		Chat: &tele.Chat{ID: chatID},
-		Text: "Бот подключен, все работает как надо!👌\n\n👍Ты можешь получать уведомления, если кто-то удалит или изменит сообщения в личных чатах \n👍Ты можешь скачивать фото, видео, голосовые сообщения и кружочки которые обычно исчезают после одного просмотра\n👍У тебя есть возможность восстановить чат даже после его удаления \n\nВ общем, полный контроль над собеседником!",
-		Entities: tele.Entities{
-			{Type: tele.EntityBold, Offset: 0, Length: 37},
-			{Type: tele.EntityBold, Offset: 37, Length: 2},
-			{Type: tele.EntityCustomEmoji, Offset: 37, Length: 2, CustomEmojiID: "5463423955014529788"},
-			{Type: tele.EntityBold, Offset: 39, Length: 1},
-			{Type: tele.EntityCustomEmoji, Offset: 41, Length: 2, CustomEmojiID: "5465465194056525619"},
-			{Type: tele.EntityCustomEmoji, Offset: 132, Length: 2, CustomEmojiID: "5465465194056525619"},
-			{Type: tele.EntityCustomEmoji, Offset: 245, Length: 2, CustomEmojiID: "5465465194056525619"},
-		},
-	}
+	messageBuilder := telegram.MessageBuilder{Mdv2Enabled: true}
+
+	messageBuilder.WriteString(
+		"Бот подключен, все работает как надо!", telegram.TextFormat{Type: telegram.TextFormatTypeBold},
+	).WriteString(
+		"👌",  telegram.TextFormat{Type: telegram.TextFormatTypeLink}.WithCustomEmojiID("5463423955014529788"),
+	).WriteString(
+		"\n\nТеперь:\n",
+	).WriteString(
+		"👍", telegram.TextFormat{Type: telegram.TextFormatTypeLink}.WithCustomEmojiID("5465465194056525619"),
+	).WriteString(
+		"Ты будешь получать уведомления, если кто-то удалит или изменит сообщения в личных чатах \n",
+	).WriteString(
+		"👍", telegram.TextFormat{Type: telegram.TextFormatTypeLink}.WithCustomEmojiID("5465465194056525619"),
+	).WriteString(
+		"Ты сможешь скачивать фото, видео, голосовые сообщения и кружочки которые обычно исчезают после одного просмотра\n",
+	).WriteString(
+		"👍", telegram.TextFormat{Type: telegram.TextFormatTypeLink}.WithCustomEmojiID("5465465194056525619"),
+	).WriteString(
+		"У тебя будет возможность восстановить чат даже после его удаления \n\nВ общем, полный контроль над собеседником!",
+	)
+
+	return messageBuilder.Build(chatID)
 }
 
+// Build message about bot disconnected
+//
+// Takes: chat ID
+//
+// Returns: message
 func buildDisconnectedMessage(chatID int64) *tele.Message {
-	botMentionOffset := utils.TgLen("Бот отключён!🙈\n\nБольшая часть функций недоступна. Бот будет работать только в тех чатах, где собеседник использует ")
-	return &tele.Message{
-		Chat: &tele.Chat{ID: chatID},
-		Text: "Бот отключён!🙈\n\nБольшая часть функций недоступна. Бот будет работать только в тех чатах, где собеседник использует " + shared.BotUsername,
-		Entities: tele.Entities{
-			{Type: tele.EntityBold, Offset: 0, Length: 13},
-			{Type: tele.EntityBold, Offset: 13, Length: 2},
-			{Type: tele.EntityCustomEmoji, Offset: 13, Length: 2, CustomEmojiID: "5463345378587849154"},
-			{Type: tele.EntityBold, Offset: 15, Length: 1},
-			{Type: tele.EntityMention, Offset: botMentionOffset, Length: utils.TgLen(shared.BotUsername)},
-		},
-		ReplyMarkup: &tele.ReplyMarkup{
-			InlineKeyboard: [][]tele.InlineButton{
-				{{Text: "Показать список", Data: shared.UniqueShowContacts}},
-			},
-		},
-	}
+	messageBuilder := telegram.MessageBuilder{Mdv2Enabled: true}
+	
+	messageBuilder.WriteString(
+		"Бот отключён!", telegram.TextFormat{Type: telegram.TextFormatTypeBold},
+	).WriteString(
+		"🙈", telegram.TextFormat{Type: telegram.TextFormatTypeLink}.WithCustomEmojiID("5463345378587849154"),
+	).WriteString(
+		"\n\nБольшая часть функций недоступна. Бот будет работать только в тех чатах, где собеседник использует ",
+	).WriteString(shared.BotUsername)
+
+	return messageBuilder.Build(chatID)
 }
 
 func run(update tele.Update, hashe *h.HandlerChainHashe) *e.ErrorInfo {
